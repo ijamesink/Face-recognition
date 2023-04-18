2db503403323a0950e65f48d7f0e7e844bb3ef9a
James Inkoom <James.Inkoom@icloud.com>
2023-04-18 00:16:52 +0000

    Updated and created new components

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -1,21 +1,131 @@
 import React, { Component } from 'react';
 import './App.css';
+// import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/navigation';
+import SignIn from './components/SignIn/Signin';
+import Register from './components/Register/Register';
 import Logo from './components/Logo/logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './Rank/rank';
+import ImageRecognition from './components/FaceRecognition/imageRecognition';
+
+// const app = new Clarifai.App({
+//   apiKey: '04bbbc84e73647078cf27d12d433f9eb'
+// });
+
+const setClarifai = (imageUrl) => {
+  const PAT = 'dee35d8cf060492a99211e093a410b21';
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = 'saint';       
+  const APP_ID = 'smart';
+  // Change these to whatever model and image URL you want to use
+  const MODEL_ID = 'face-detection';
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                }
+            }
+        }
+    ]
+  })
+
+  const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw    
+  }
+
+  return requestOptions
+}
+
 
 class App extends Component {
+  constructor () {
+    super ();
+      this.state = {
+        input: '',
+        imageUrl: '',
+        box: {},
+        route: 'signin',
+        isSignedIn: false
+      }
+  };
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].regions_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
+
+  displayFaceBox = (box) => {
+    this.setState({box: box})
+  }
+
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  };
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", setClarifai(this.state.input))
+      .then(response => response.json())
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(error => console.log('error', error));
+   
+  };
+
   render() {
+    const { isSignedIn, box, imageUrl, route } = this.state;
     return (
       <div className="App">
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm />
-                {/* {<logo />
-        <ImageLinkForm />
-        <FaceRecognition />} */}
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        { route === 'home' 
+          ? <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit} 
+              />
+              <ImageRecognition box={box} imageUrl={imageUrl} />
+            </div>
+          : (
+            route === 'signin'
+            ? <SignIn onRouteChange={this.onRouteChange}/>
+            : <Register onRouteChange={this.onRouteChange}/>
+          )
+        }
       </div>
     );
   } 
