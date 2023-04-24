cd03b17aa34eb3b9b3b61bae46118e1778fdbc4f
James Inkoom <James.Inkoom@icloud.com>
2023-04-24 19:36:59 +0000

    added functionalities to signin, rank, register and app

--- facerecognition/src/Rank/rank.js
+++ facerecognition/src/Rank/rank.js
@@ -2,14 +2,14 @@ import React from "react";
 
 
 
-const Rank = () => {
+const Rank = ({ name, entries }) => {
     return (
         <div>
              <div className="white f3">
-                {'James, your current rank is...'}
+                {'${name}, your current entry count is: '}
              </div>
              <div className="white f1">
-                {'#2'}
+                {entries}
              </div>
         </div>
         
